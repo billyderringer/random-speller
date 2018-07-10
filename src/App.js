9366c34de2e3914abd46a08ac01941d2eaf2d413
Billy Derringer <billyderringer7@gmail.com>
2018-07-10 13:32:44 -0500

    changed api env

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import Header from "./components/Header";
-import Content from "./components/Content/Content";
-import Footer from "./components/Footer";
-import Loader from "./components/Loader";
+import Header from "./components/Header"
+import Content from "./components/Content/Content"
+import Footer from "./components/Footer"
+import Loader from "./components/Loader"
+require('dotenv').config()
 
 //**used to override responsive voice api logs
 //console.log = function() {}
@@ -28,11 +29,12 @@ class App extends Component {
     //Get random word from words api
     //sent to Content component for voice
     getNewWord() {
+        console.log(process.env)
         axios({
             method: 'get',
             url:'https://wordsapiv1.p.mashape.com/words/?random=true&letterpattern=/^\\S*$/',
             headers: {
-                "X-Mashape-Key": "lS563P9svKmshFblGwDm0QmxMQiip16Cf3yjsnwebKjRFbSiM3",
+                "X-Mashape-Key": process.env.REACT_APP_X_MASHAPE_KEY,
                 "X-Mashape-Host": "wordsapiv1.p.mashape.com"
             }
         }).then((res) => {
